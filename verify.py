1bb4ac07e08a174247a36e2a0bf0d0f2b5379594
Valentin Roland <valentin@vroland.de>
2021-06-22 15:20:30 +0200

    verify projection claims

--- verify.py
+++ verify.py
@@ -12,6 +12,8 @@ component_models = defaultdict(list)
 component_projections = defaultdict(list)
 # clauses by covered by component
 component_clauses = {}
+# list of join claims by component
+component_joins  = defaultdict(list)
 
 def parse_proof(input_file):
     for line in input_file.readlines():
@@ -29,22 +31,24 @@ def parse_proof(input_file):
             clauses_dict[l_id] = frozenset(l_data)
 
         elif l_type == "cv":
-            component_variables[l_id] = set(l_data)
+            component_variables[l_id] = frozenset(l_data)
 
         elif l_type == "cd":
-            component_clauses[l_id] = set([clauses_dict[c] for c in l_data])
+            component_clauses[l_id] = frozenset([clauses_dict[c] for c in l_data])
 
         elif l_type == "m":
             component_models[l_id].append(frozenset(l_data))
 
         elif l_type == "p":
-            component_projections[l_id].append((l_count, l_data))
+            component_projections[l_id].append((l_count, frozenset(l_data)))
 
-    # true for DP, but not for component caching
-    #def get_component_clauses(component_id):
-    #    variables = component_variables[component_id]
-    #    clauses = filter(lambda clause: set([abs(l) for l in clause]) <= variables, clauses_dict.values())
-    #    return set(clauses)
+        elif l_type == "j":
+            component_joins[l_id].append((l_count, frozenset(l_data)))
+
+    # insert empty projection component
+    component_clauses[-1] = frozenset()
+    component_projections[-1] = [(1, frozenset())]
+    component_variables[-1] = frozenset()
 
     #component_clauses = { c_id : get_component_clauses(c_id) for c_id in component_variables.keys() }
 
@@ -60,7 +64,7 @@ def map_lit(variable_mapping, l):
     return var if l > 0 else -var
 
 def check_model_claims(component_id):
-    clauses = component_clauses[component]
+    clauses = set(component_clauses[component])
 
     # add negated models -> -(a ^ b) => (-a or -b)
     for model in component_models[component]:
@@ -87,29 +91,92 @@ def check_model_claims(component_id):
         print (problem_string)
         sys.exit(2)
 
-def largest_subclaim_cover(component_id):
+def largest_subprojection(component_id):
     covered_clauses = component_clauses[component_id]
+    component_vars = component_variables[component_id]
 
     # find sub-projection
-    best_subprojection = 0
-    best_subprojection_clauses = set()
-    for c_id, c_vars in component_variables.items():
+    best_subprojection = -1
+    # tuple of (matching clauses, matching variables)
+    best_subprojection_value = (-1, -1)
+    for c_id in set(component_projections.keys()) | set(component_joins.keys()):
         if c_id == component_id:
             continue
 
+        comp_vars = component_variables[c_id]
         comp_clauses = component_clauses[c_id]
         common = comp_clauses & covered_clauses
-        if comp_clauses <= covered_clauses and len(common) > len(best_subprojection_clauses):
+        comp_value = (len(common), len(component_vars & comp_vars))
+        if comp_clauses <= covered_clauses and comp_vars <= component_vars and comp_value > best_subprojection_value:
+
             best_subprojection = c_id
-            best_subprojection_clauses = common
+            best_subprojection_value = comp_value
 
-    if not best_subprojection:
-        print ("leaf projection?", component_id)
+    return best_subprojection
+
+def check_projection(comp_proj, comp_source, comp_bridge):
+    source_assignments = component_projections[comp_source] if comp_source in component_projections else component_joins[comp_source]
+
+    source_vars = component_variables[comp_source]
+
+    for c_proj, a_proj in component_projections[comp_proj]:
+        c_proj_check = 0
+        for bridge_assignment in component_models[comp_bridge]:
+            # the projected assignment must be a restriction of the bridge_assignment
+            if not a_proj <= bridge_assignment:
+                continue
+
+            # bridge assignment restricted to source component variables
+            restricted_bridge_assignment = set([l for l in bridge_assignment if abs(l) in source_vars])
+
+            for c_source, a_source in source_assignments:
+                # this assignment fits to the restricted bridge assignment
+                if restricted_bridge_assignment <= a_source:
+                    c_proj_check += c_source
+
+        if c_proj_check != c_proj:
+            print ("check for", comp_proj, comp_source, "failed:", *a_proj, ":", c_proj_check, "<->", c_proj)
+            return False
+    return True
+
+def check_projection_claim(component_id):
+    # the trivial projection is valid
+    if component_id == -1:
+        return True
+
+    predecessor = largest_subprojection(component_id)
+    assert predecessor != 0
+
+    comp_vars = component_variables[component_id]
+    pred_vars = component_variables[predecessor]
+
+
+    # we must find a model claim that add the valid
+    # assignments for the missing clauses
+    clauses_needed = component_clauses[component_id] - component_clauses[predecessor]
+
+    assert predecessor in component_projections or predecessor in component_joins
+    assert comp_vars > pred_vars
+
+    introduce_vars = comp_vars - pred_vars
+
+    bridge_component = 0
+    for key, value in component_clauses.items():
+        candidate_vars = component_variables[key]
+        # the current component adds additional clauses
+        if clauses_needed and value == clauses_needed and candidate_vars <= comp_vars:
+            bridge_component = key
+            break
+        # no clauses added
+        elif not clauses_needed and candidate_vars == introduce_vars:
+            bridge_component = key
+            break
     else:
-        print ("best subprojection:", component_id, "->", best_subprojection, "remaining clauses:", covered_clauses - best_subprojection_clauses)
+        print ("no bridge component found for", component_id, predecessor)
+        return False
 
-def check_projection_claim(component_id, count, partial_assignment):
-    largest_subclaim_cover(component_id)
+    return check_projection(component_id, predecessor, bridge_component)
+
 
 if __name__ == "__main__":
     parse_proof(sys.stdin)
@@ -123,6 +190,9 @@ if __name__ == "__main__":
     print ("model claims verified (according to minisat).", file=sys.stderr)
 
     for component, l in component_projections.items():
-        for (c, a) in l:
-            check_projection_claim(component, c, a)
-            break
+        checked = check_projection_claim(component)
+        if checked:
+            print ("projection claim", component, "verified.")
+        else:
+            print ("projection claim", component, "failed!")
+            sys.exit(1)
